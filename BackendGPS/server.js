86d2c93d2a2fb06b6c9c67865c01d8fd6c70621e
TheXvis <charly.brantt1901@alumnos.ubiobio.cl>
2024-03-21 22:18:01 -0300

    bdd connection

--- BackendGPS/server.js
+++ BackendGPS/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const router = require('./routes/fileRoutes');
+require('dotenv').config();
 
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e));
+
+
+  
 app.use('/', router);
 
 app.listen(port, () => {
