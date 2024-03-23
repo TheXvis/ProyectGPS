c58bc369f6e1d557a30a8b2fad012623c0f2a6a7
TheXvis <charly.brantt1901@alumnos.ubiobio.cl>
2024-03-23 14:47:40 -0300

    server fix

--- BackendGPS/server.js
+++ BackendGPS/server.js
@@ -3,14 +3,22 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 const userRoutes = require('./routes/userRoutes');
+const carrierRoutes = require('./routes/carrierRoutes');
+
 require('dotenv').config();
 
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri).then(() => console.log('Base de datos conectada')).catch(e => console.log(e));
+mongoose.connect(uri, {
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4, 
+}).then(() => console.log('Base de datos conectada')).catch(e => console.log(e));
 
 app.use('/user', userRoutes);
+app.use('/carrier', carrierRoutes);
+
 
 app.listen(port, () => {
   console.log(`Aplicación escuchando en http://localhost:${port}`);
