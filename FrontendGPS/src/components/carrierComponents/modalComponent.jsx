ef8cfddcef542e87a5e718298309b07d510505a5
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2024-04-22 18:12:47 -0400

    modal funcional y reutilizable(de momento), imports a React agregados

---
+++ FrontendGPS/src/components/carrierComponents/modalComponent.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = ({ children, isOpen, onClose }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="fixed inset-0 z-50 overflow-y-auto bg-gray-500 bg-opacity-75 p-4 md:p-8">
+            <div className="relative bg-white rounded-lg shadow-dark md:w-1/2 mx-auto">
+                <button type="button" className="absolute top-3 right-2.5 text-gray-400 hover:bg-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={onClose}>
+                    <span className="sr-only">Close</span>
+                    <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10l-4.293-4.293z" clipRule="evenodd" />
+                    </svg>
+                </button>
+                <div className="p-6 text-center">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Modal.propTypes = {
+    children: PropTypes.node.isRequired,
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
