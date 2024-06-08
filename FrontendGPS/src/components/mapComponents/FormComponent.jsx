def9fc13d1529b50dd8097ed9b9745471b44adae
rodrigopgg <rodrigoparra08@gmail.com>
2024-06-07 22:58:04 -0400

    funion de sugerencia quitada (muy imprecisa)

--- FrontendGPS/src/components/mapComponents/FormComponent.jsx
+++ FrontendGPS/src/components/mapComponents/FormComponent.jsx
@@ -1,15 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import InputField from "./InputField";
 
 function FormComponent() {
 	const [origin, setOrigin] = useState("");
 	const [destination, setDestination] = useState("");
-	const [originSuggestions, setOriginSuggestions] = useState([]);
-	const [destinationSuggestions, setDestinationSuggestions] = useState([]);
 	const [location, setLocation] = useState(null);
-	const [houseNumber, setHouseNumber] = useState(null);
-
-	const countryCode = "CL";
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
@@ -22,28 +17,6 @@ function FormComponent() {
 		);
 	}, []);
 
-	const fetchSuggestions = (query, setSuggestions) => {
-		if (!query) {
-			setSuggestions([]);
-			return;
-		}
-
-		let url = `https://nominatim.openstreetmap.org/search?q=${query}&countrycodes=${countryCode}&format=json&addressdetails=0&limit=5`;
-		if (location) {
-			const [lat, lon] = location;
-			const viewbox = `${lon - 10},${lat + 25},${lon + 10},${lat - 25}`;
-			url += `&viewbox=${viewbox}`;
-		}
-		fetch(url)
-			.then((response) => response.json())
-			.then((data) => {
-				setSuggestions(data);
-			})
-			.catch((error) => {
-				console.error("Error fetching suggestions:", error);
-			});
-	};
-
 	return (
 		<form className="space-y-4" style={{ height: "19vh", width: "100%" }}>
 			<InputField
@@ -51,24 +24,12 @@ function FormComponent() {
 				placeholder="Origen"
 				value={origin}
 				setValue={setOrigin}
-				suggestions={originSuggestions}
-				setSuggestions={setOriginSuggestions}
-				fetchSuggestions={fetchSuggestions}
-				location={location}
-				houseNumber={houseNumber}
-				setHouseNumber={setHouseNumber}
 			/>
 			<InputField
 				id="destination"
 				placeholder="Destino"
 				value={destination}
 				setValue={setDestination}
-				suggestions={destinationSuggestions}
-				setSuggestions={setDestinationSuggestions}
-				fetchSuggestions={fetchSuggestions}
-				location={location}
-				houseNumber={houseNumber}
-				setHouseNumber={setHouseNumber}
 			/>
 		</form>
 	);
--- FrontendGPS/src/components/mapComponents/InputField.jsx
+++ FrontendGPS/src/components/mapComponents/InputField.jsx
@@ -1,40 +1,9 @@
-import React, { useRef } from "react";
+import React from "react";
 import Button from "./Button";
 
-function InputField({
-	id,
-	placeholder,
-	value,
-	setValue,
-	suggestions,
-	setSuggestions,
-	fetchSuggestions,
-	location,
-	houseNumber,
-	setHouseNumber,
-}) {
-	const timer = useRef(null);
-
+function InputField({ id, placeholder, value, setValue }) {
 	const handleInputChange = (event) => {
-		var query = event.target.value;
-		var houseNumberMatch = query.match(/\d+/);
-		setHouseNumber(houseNumberMatch ? houseNumberMatch[0] : "");
-		setValue(query);
-		clearTimeout(timer.current);
-		timer.current = setTimeout(() => {
-			query = query.replace(/\s/g, "%20");
-			fetchSuggestions(query, setSuggestions);
-		}, 1500);
-	};
-
-	const handleSuggestionClick = (suggestion) => {
-		setValue(`${houseNumber} ${suggestion.display_name}`);
-	};
-
-	const handleBlur = () => {
-		setTimeout(() => {
-			setSuggestions([]);
-		}, 200);
+		setValue(event.target.value);
 	};
 
 	return (
@@ -47,21 +16,10 @@ function InputField({
 				value={value}
 				className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-10 mb-1 mr-2"
 				onChange={handleInputChange}
-				onBlur={handleBlur}
 			/>
 			<Button iconSrc="send" title="Enviar dirección" />
 			<Button iconSrc="map" title="Ubicar en el mapa" />
 			<Button iconSrc="gps" title="Ubicar con GPS" />
-			<ul className="absolute top-full left-0 w-full bg-white z-10">
-				{suggestions.slice(0, 5).map((suggestion, index) => (
-					<li
-						key={index}
-						className="suggestion gb-white-100 dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-						onClick={() => handleSuggestionClick(suggestion)}>
-						{`${houseNumber} ${suggestion.display_name}`}
-					</li>
-				))}
-			</ul>
 		</div>
 	);
 }
