5f17ae29c8250ce1fa7c5c12c521d5553acc816c
William <xvisindomable@gmail.com>
2024-07-24 19:10:09 -0400

    fix strim and estado default

--- FrontendGPS/src/pages/User/publicationDetailPage.jsx
+++ FrontendGPS/src/pages/User/publicationDetailPage.jsx
@@ -110,14 +110,14 @@ function PublicationDetailsPage() {
         <p className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Peso: {publication.peso}</p>
         <p className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Estado: {publication.estado}</p>
 
-        {userRole === 'carrier' && publication.estado.trim() == 'Disponible' && (
+        {userRole === 'carrier'&& publication && publication.estado && publication.estado.trim() == 'Disponible' && (
           <>
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={solucitarPublicacion}>Solicitar Publicación</button>
           </>
           
         )}
-        {userRole === 'carrier' && publication.estado.trim() == 'Aceptado' && (
+        {userRole === 'carrier'&& publication && publication.estado && publication.estado.trim() == 'Aceptado' && (
           <>
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={iniciarViaje}>Iniciar viaje</button>
@@ -125,7 +125,7 @@ function PublicationDetailsPage() {
           
         )}
 
-        {userRole === 'carrier' && publication.estado.trim() == 'En transito' && (
+        {userRole === 'carrier'&& publication && publication.estado && publication.estado.trim() == 'En transito' && (
           <>
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={finalizarViaje}>Iniciar viaje</button>
@@ -142,7 +142,7 @@ function PublicationDetailsPage() {
         </>
         )}
 
-        {userRole === 'user' && publication.estado.trim() == 'Finalizado' && (
+        {userRole === 'user'&& publication && publication.estado && publication.estado.trim() == 'Finalizado' && (
         <>
           <div>
             <ReseñaForm/>
@@ -151,7 +151,7 @@ function PublicationDetailsPage() {
         )}
 
       </div>
-      {userRole === 'user' && publication.estado.trim() == 'Pendiente' &&(
+      {userRole === 'user'&& publication && publication.estado && publication.estado.trim() == 'Pendiente' &&(
     <div className="justify-center  bg-white dark:bg-gray-800 p-2 shadow inline-block text-center mt-4 ml-21 items-center">
       <div>
         <p className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">RUT del Carrier: {publication.rutCarrier}</p>
