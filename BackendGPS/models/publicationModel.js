ae19f3f1520e9621a14d840da27e72026ee39dea
Zeev7n <gabriela.carrasco1901@alumnos.ubiobio.cl>
2024-07-25 17:16:59 -0400

    publicationModel

--- BackendGPS/models/publicationModel.js
+++ BackendGPS/models/publicationModel.js
@@ -8,10 +8,31 @@ const publicationSchema = new Schema({
     tipoMercancia: { type: String, required: true },
     imagen: { type: String },
     peso: { type: Number, required: true },
-    precio: { type: Number, required: false },
-    ubicacionCarga: { type: Schema.Types.Mixed, required: false },
-    ubicacionDescarga: { type: Schema.Types.Mixed, required: false },
-    estado: { type: String},
+    precio: { type: Number, required: true },
+    ubicacionCarga: { type: String, required: true },
+    ubicacionDescarga: { type: String, required: true },
+    estado: { type: String, required: true, default: 'Disponible' },
   });
 
 module.exports = mongoose.model('Publication', publicationSchema);
+
+/* const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const publicationSchema = new Schema({
+    rutUser: { type: String, required: true },
+    rutCarrier: { type: String },
+    nombre: { type: String, required: true },
+    tipoMercancia: { type: String, required: true }, //Cargas pesadas-Fragiles-Perecederas-Quimicos-etc
+    imagen: { type: String },
+    peso: { type: Number, required: true },
+    volumen: { type: Number, required: true },
+    precio: { type: Number, required: true },
+    ubicacion: { type: String, required: true }, //Ubicacion Actual
+    origen: { type: String, required: true }, //Ubicacion de origen de la carga
+    destino: { type: String, required: true }, //Ubicacion de destino de la carga
+    estado: { type: String}, //Publicada-Aceptada-En camino-Entregada
+    condicionesEspeciales: { type: String }, //Costo elevado-Entrega rapida-Congelados -etc
+  });
+
+module.exports = mongoose.model('Publication', publicationSchema); */
