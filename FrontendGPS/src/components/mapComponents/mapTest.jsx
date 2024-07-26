b6ca94ee03cf2f174bc8991600f67a7865537c4a
rodrigopgg <rodrigoparra08@gmail.com>
2024-07-25 21:13:12 -0400

    Portada + fix sidebar

--- FrontendGPS/src/components/mapComponents/mapTest.jsx
+++ FrontendGPS/src/components/mapComponents/mapTest.jsx
@@ -5,7 +5,9 @@ import LeafletRoutingMachine from "../../mapServices/LeafletRoutingMachine";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { calculatePrice } from "../../mapServices/priceCalculator";
-import L from "leaflet";
+import { useNavigate } from "react-router-dom";
+
+//import L from "leaflet";
 
 function MapComponent({ originPosition, destinationPosition }) {
 	const location = useLocation();
@@ -16,8 +18,10 @@ function MapComponent({ originPosition, destinationPosition }) {
 	const [zoom, setZoom] = useState(6); // Default zoom level if no positions are set
 	const [price, setPrice] = useState(100); // Example fixed price
 	const [showModal, setShowModal] = useState(false); // State to control modal visibility
+	const [showModalVerification, setShowModalVerification] = useState(false); // modal para confirmr que se public correctamente
 	const [distance, setDistance] = useState(0); // Distance of the trip
 	const mapRef = useRef(null);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
@@ -56,8 +60,13 @@ function MapComponent({ originPosition, destinationPosition }) {
 					precio: price,
 				}
 			);
+
 			// Redireccionar a mis publicaciones
 			setShowModal(false); // Cerrar el modal después de publicar
+			// si se publica correctamente, mostrar el modal de verificacion
+			if (response.status === 200){
+				setShowModalVerification(true); // Abre el modal de confirmacio
+			}
 		} catch (error) {
 			console.error("Error updating publication:", error);
 			alert("Hubo un error al publicar el viaje");
@@ -104,16 +113,16 @@ function MapComponent({ originPosition, destinationPosition }) {
 				</button>
 			</div>
 
-			{/* Modal de Confirmación */}
+			{/* Modal pra confirmar pblicación */}
 			{showModal && (
 				<div className="fixed inset-0 flex items-center justify-center z-50">
 					<div className="fixed inset-0 bg-gray-600 bg-opacity-50"></div>
 					<div className="bg-white p-6 rounded-lg shadow-lg z-10">
 						<h2 className="text-lg font-bold mb-4">Confirmación</h2>
 						<p className="mb-4">
-							¿Estás seguro de que deseas publicar este viaje? El precio será $
-							{price}.
+							¿Estás seguro de que deseas publicar este viaje?
 						</p>
+						<p className="bd-4">El precio sera $ {price}</p>
 						<div className="flex justify-end">
 							<button
 								onClick={confirmPublish}
@@ -124,6 +133,29 @@ function MapComponent({ originPosition, destinationPosition }) {
 								onClick={() => setShowModal(false)}
 								className="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-700">
 								Cancelar
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+			{/*Modal de verificaion */}
+			{showModalVerification && (
+				<div className="fixed inset-0 flex items-center justify-center z-50">
+					<div className="fixed inset-0 bg-gray-600 bg-opacity-50"></div>
+					<div className="bg-white p-6 rounded-lg shadow-lg z-10">
+						<h2 className="text-lg font-bold mb-4">
+							La publicacion fue publicada exitosamente
+						</h2>
+						<p className="mb-4"></p>
+						<div className="flex justify-end">
+							<button
+								// redirigir a  navigate('/usuario-home'); y cerrar el modal
+								onClick={() => {
+									setShowModalVerification(false);
+									navigate("/usuario-home");
+								}}
+								className="bg-blue-500 text-white px-4 py-2 rounded-lg mr-2 hover:bg-blue-700">
+								Coninuar
 							</button>
 						</div>
 					</div>
--- FrontendGPS/src/pages/User/userPage.jsx
+++ FrontendGPS/src/pages/User/userPage.jsx
@@ -1,11 +1,23 @@
-import SideBar from '../../components/sideBar';
+
 
 function UserPage() {
-    return (
-      <div className="bg-gray-50 dark:bg-gray-900 min-h-screen">
-        <SideBar />
+  return (
+    <div className="bg-gray-50 dark:bg-gray-900 min-h-screen flex">
+      {/* <SideBar /> */}
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <div className="text-center mb-6">
+          <h1 className="text-5xl text-white font-bold mb-4">Bienvenido Usuario</h1>
+          <p className="text-lg text-gray-300">Aquí puedes gestionar tus publicaciones y más</p>
+        </div>
+        {/* espaciador */}
+        <div className="h-15  "></div>
+        <div className="flex justify-center items-center space-x-6">
+          <img src="src/assets/Portada/CamionPortada.png" alt="Camion" className="w-80 h-auto object-contain shadow-lg rounded-lg" />
+          <img src="src/assets/Portada/RutaGPSPortada.png" alt="Ruta GPS" className="w-80  h-auto object-contain shadow-lg rounded-lg" />
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default UserPage;
